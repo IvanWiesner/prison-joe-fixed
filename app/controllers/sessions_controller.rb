d852ac956311cf8ed623b14992f7f60684a2d172
Ivan <ivanwiesner@gmail.com>
2021-09-15 15:28:17 -0600

    heroku

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-        skip_before_action :authorize, only: [:create, :index]
+        # skip_before_action :authorize, only: [:create, :index]
     
         def index
             cookies[:cookies_hi] ||= 'test cookie'
