7f4133c453b1a40e8b0f26f2503f1372d26cc164
Ivan <ivanwiesner@gmail.com>
2021-09-17 12:38:23 -0600

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,9 +20,6 @@ puts "merch start"
 Merch.create(name: "Soap Hat", image_url: "https://i.imgur.com/c5gTq0k.jpg", price: 27)
 Merch.create(name: "Slot 2", image_url: "https://i.imgur.com/0mEPz3C.png", price: "TBD")
 Merch.create(name: "Slot 3", image_url: "https://i.imgur.com/0mEPz3C.png", price: "TBD")
-Merch.create(name: "Slot 4", image_url: "https://i.imgur.com/0mEPz3C.png", price: "TBD")
-Merch.create(name: "Slot 5", image_url: "https://i.imgur.com/0mEPz3C.png", price: "TBD")
-Merch.create(name: "Slot 6", image_url: "https://i.imgur.com/0mEPz3C.png", price: "TBD")
 
 puts "merch done"
 
