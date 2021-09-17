6c63cd80baace80400ff34ea288c9f0fc8ac50a9
Ivan <ivanwiesner@gmail.com>
2021-09-17 13:03:55 -0600

    done

--- client/src/components/MerchPage.js
+++ client/src/components/MerchPage.js
@@ -1,6 +1,5 @@
 import React, {useState, useEffect}  from "react"
 import MerchCard from "./MerchCard";
-import Button from "react-bootstrap/button";
 
 
 function MerchPage({merches, setMerches, addToCart, login}) {
