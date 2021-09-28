3dcbf3e414bf3b4bc1523420b9e42a92214ccab5
Ivan <ivanwiesner@gmail.com>
2021-09-28 11:32:10 -0600

    t

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,6 +37,7 @@ class UsersController < ApplicationController
         end
         
         private 
+        
         def user_params 
             params.permit(:email, :password)
         end
