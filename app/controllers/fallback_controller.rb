e07e739aa368b28140d086df349cc4db8fa98489
Ivan <ivanwiesner@gmail.com>
2021-09-29 14:00:02 -0600

    fallback

--- app/controllers/fallback_controller.rb
+++ app/controllers/fallback_controller.rb
@@ -1,5 +1,12 @@
 class FallbackController < ApplicationController
+    # def index
+    #     render file: 'public/index.html'
+    #   end
+    include ActionController::MimeResponds
+
     def index
-        render file: 'public/index.html'
-      end
+        respond_to do |format|
+            format.html { render body: Rails.root.join('public/index.html').read }
+        end
+    end
 end
