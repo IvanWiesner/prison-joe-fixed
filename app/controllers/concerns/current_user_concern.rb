ce8bbf98b12cca4a51dc3eb4164fb9eec419f941
IvanWiesner <ivanwiesner@gmail.com>
2021-11-05 08:53:31 -0400

    auth

---
+++ app/controllers/concerns/current_user_concern.rb
@@ -0,0 +1,14 @@
+module CurrentUserConcern
+    # extend
+    extend ActiveSupport::Concern
+
+    included do 
+        before_action :set_made_user
+    end
+
+    def set_made_user
+        if session[:user_id]
+            @current_user = User.find(session[:user_id])
+        end
+    end
+end
